160a09598834e157b4f5a623085faf1233c3f009
W-M <w-m@gmx.us>
2016-03-28 18:23:47 +0200

    Sequences for square roots

--- mix.exs
+++ mix.exs
@@ -31,7 +31,8 @@ defmodule Sequences.Mixfile do
   defp deps do
     [
       {:earmark, ">= 0.0.0", only: [:dev]}, # Markdown, dependency of ex_doc
-      {:ex_doc, "~> 0.11", only: [:dev]}    # Documentation for Hex.pm
+      {:ex_doc, "~> 0.11", only: [:dev]},    # Documentation for Hex.pm
+      {:math, git: "https://github.com/qqwy/math", tag: "v0.0.2"}
     ]
   end
 
