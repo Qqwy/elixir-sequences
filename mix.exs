a85a76dd1e990b2d424294e13d127ed836943de4
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2016-12-14 20:58:38 +0100

    Fix Travis-CI config

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sequences.Mixfile do
   def project do
     [app: :sequences,
      version: "1.2.0",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: description,
